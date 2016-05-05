a1cca4b96a39114997bab5dd979e1da5e9b72f7c
jschelke <Jeroen@Jeroens-MacBook-Pro.local>
2016-05-05 17:07:08 +0200

    Jeroen
    Starting to centralise variables

--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -7,43 +7,32 @@ import java.awt.Image;
 import java.awt.event.*;
 
 public class MenuPanel extends JPanel implements ActionListener {
-	private JButton SingleplayerButton, MultiplayerButton, QuitButton, DesertButton, MountainButton, PlainButton;
+	private JButton PlayButton, QuitButton;
 	private Tanks mainscreen;
+	private JComboBox environment;//kies kleur terrein
+	private String[] environmentChoices = {"Plains","Desert", "Arctic"};// keuzes voor de ComboBox
+	private int environmentChoice = 0;
 
 	public MenuPanel(Tanks mainscreen){	
 		this.mainscreen = mainscreen;
 		
-		SingleplayerButton = new JButton("Singleplayer");
-		SingleplayerButton.addActionListener(this);
-
-		MultiplayerButton = new JButton("Multiplayer");
-		MultiplayerButton.addActionListener(this);
+		PlayButton = new JButton("Play");
+		PlayButton.addActionListener(this);
 
 		QuitButton = new JButton("Quit");
 		QuitButton.addActionListener(this);
 		
-		DesertButton = new JButton("Desert");
-		DesertButton.addActionListener(this);
+		environment = new JComboBox(environmentChoices);
+		environment.setSelectedIndex(0);
+		environment.addActionListener(this);
 		
-		MountainButton = new JButton("Mountains");
-		MountainButton.addActionListener(this);
-		
-		PlainButton = new JButton("Plain");
-		PlainButton.addActionListener(this);
 
-		SingleplayerButton.setPreferredSize(new Dimension(130, 30));
-		MultiplayerButton.setPreferredSize(new Dimension(130, 30));
+		PlayButton.setPreferredSize(new Dimension(130, 30));
 		QuitButton.setPreferredSize(new Dimension(130, 30));
-		DesertButton.setPreferredSize(new Dimension(130,30));
-		MountainButton.setPreferredSize(new Dimension(130,30));
-		PlainButton.setPreferredSize(new Dimension(130,30));
 
-		this.add(SingleplayerButton);
-		this.add(MultiplayerButton);
+		this.add(PlayButton);
 		this.add(QuitButton);
-		this.add(DesertButton);
-		this.add(MountainButton);
-		this.add(PlainButton);
+		this.add(environment);
 	}
 
 	public void paintComponent(Graphics g) {
@@ -53,16 +42,19 @@ public class MenuPanel extends JPanel implements ActionListener {
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == SingleplayerButton) {
+		if (e.getSource() == PlayButton) {
 			mainscreen.switchPanel(new PlayPaneel());
-
-		} else if (e.getSource() == MultiplayerButton) {
-			mainscreen.switchPanel(new PlayPaneel());
-
+		} else if (e.getSource() == environment) {
+			JComboBox cb = (JComboBox)e.getSource();
+			String selection =  (String) ((JComboBox) e.getSource()).getSelectedItem();
+			for(int i=0;i<environmentChoices.length;i++){
+				if(environmentChoices[i]==selection){
+					environmentChoice = i;
+				}
+			}
 		} else {
 			System.exit(0);
 		}
-
 	}
 
 }
